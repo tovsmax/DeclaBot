0b13fa021aa6d695ad7a709e7efde2d8666bfbc2
tovsmax <tovsmax@gmail.com>
2022-06-06 14:27:28 +0300

    Замена либы

--- server/bot.js
+++ server/bot.js
@@ -1,24 +1,16 @@
-import telegraf, { Telegraf, Telegram } from "telegraf"
+import TelegramBot from "node-telegram-bot-api";
 
-export default function (token, pageUrl) {
-  const bot = new Telegraf(token)
+export default function(pageUrl) {
+  const token = "5329580924:AAFsd2Itl-F4PDxgOOLtwkGNzCbTst1CvH0"
+  const bot = new TelegramBot(token, {polling: true})
 
-  bot.start((ctx) => {
-    ctx.telegram.getMe().then((data) => {
-      ctx.reply(data)
-    })
-
-    const MenuButtonWebApp = {
+  bot.setChatMenuButton({
+    menu_button: {
       type: "web_app",
-      text: "My web page",
+      text: "kekos",
       web_app: {
         url: pageUrl,
-      },
+      }
     }
-    ctx.setChatMenuButton(MenuButtonWebApp)
   })
-
-  bot.launch()
-
-  return bot
 }
